43e6818fb5652a26c100d4c836ce3cb61b6bbe63
Patrik Plihal <patrik.plihal@gmail.com>
2016-07-26 12:58:18 +0200

    attemt at more idiomatic code

--- project.clj
+++ project.clj
@@ -3,10 +3,13 @@
   :url "https://github.com/ttsiodras/HexSpeak"
   :license {:name "GNU Public License"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.combinatorics "0.1.3"]
+                 [medley "0.8.2"]]
   :aot [thanassis.hexspeak]
   ; :main ^:skip-aot thanassis.hexspeak
   :main thanassis.hexspeak
   :target-path "target/%s"
   :jvm-opts ^:replace []
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[com.taoensso/tufte "1.0.0"]]}})
